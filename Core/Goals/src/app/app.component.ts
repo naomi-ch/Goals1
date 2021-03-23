b73ebdd8d717f20d0605dfebeaec6ce0e2d5e4aa
Naomi Chika <naomi.chika.1@gmail.com>
2021-03-23 19:30:51 +0300

    add goal object to component and html

--- Core/Goals/src/app/app.component.ts
+++ Core/Goals/src/app/app.component.ts
@@ -11,6 +11,7 @@
 
 
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
@@ -18,10 +19,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goals: string[];
-
-  constructor(){
-    this.goals = ['Watch Finding Nemo' , 'Buy cookies' , 'Get new phone case']
+  goals: Goal[] = [
+    {id:1, name: 'Watch Finding Nemo'},
+    {id:2, name: 'Buy Cookies'},
+    {id:3, name: 'Get New Phone Case'},
+    {id:4, name: 'Get Dog Food'},
+    {id:5, name: 'Solve Math Homework'},
+    {id:6, name: 'Plot My World Domination Plan'},
+  ];
 
   }
 }
