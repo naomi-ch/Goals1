7ec466efd390b55ff0763dbc7294dca7bebc11b9
Naomi Chika <naomi.chika.1@gmail.com>
2021-03-25 12:26:48 +0300

    add goal detail component

--- Core/Goals/src/app/goal.ts
+++ Core/Goals/src/app/goal.ts
@@ -1,4 +1,7 @@
 export class Goal {
   id!: number;
   name!: string;
+  description!: string;
 }
+
+
--- Core/Goals/src/app/goal/goal.component.ts
+++ Core/Goals/src/app/goal/goal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
 
 @Component({
   selector: 'app-goal',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+
+  goals:Goal[]= [
+    {id:1,name:'Watch Finding Nemo',description:'Find an online version and watch Merlin find his son'},
+    {id:2,name:'Buy Cookies',description:'I have to by cookies for the parrot'},
+    {id:3,name:'Get New Phone Case',description:'Diana has her birthday coming up soon'},
+    {id:4,name:'Get Dog Food',description:'Pupper likes expensive snacks'},
+    {id:5,name:'Solve Math Homework',description:'Damn Math'},
+    {id:6,name:'Plot My World Domination Plan',description:'Cause I am an evil overlord'},
+  ];
 
   constructor() { }
 
