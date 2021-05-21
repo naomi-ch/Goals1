f20735caf2161fa02c29e3c8f795bbdfbaf7aeff
Naomi Chika 1 <naomi.chika.1@gmail.com>
2021-05-21 10:18:26 +0300

    save

---
+++ Core/Week-2/Car/car.ts
@@ -0,0 +1,14 @@
+import { Engine } from './engine';
+import { Wheels } from './wheels';
+
+export class Car{
+
+
+  constructor(private engine: Engine, private wheels: Wheels){
+   
+  }
+
+  startEngine(){
+    this.engine.start();
+  }
+}
