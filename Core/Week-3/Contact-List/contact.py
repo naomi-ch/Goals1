de5206d7ad98651b3232d2c69e1686b8c0f218f8
Naomi Chika 1 <naomi.chika.1@gmail.com>
2021-05-26 13:10:25 +0300

    save

---
+++ Core/Week-3/Contact-List/contact.py
@@ -0,0 +1,20 @@
+
+class Contact:
+  """
+  Class that generates new instances of contacts
+  """
+
+  contact_list = [] 
+
+  def __init__(self,first_name,last_name,phone_number,email):
+
+    
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email 
+      
+    
+
+
+    
---
+++ Core/Week-3/Contact-List/contact_test.py
@@ -0,0 +1,12 @@
+import unittest #importing the unittest module
+from contact import Contact #importing the contact class
+
+class TestContact(unittest.TestCase):
+
+  '''
+  Test class that defines test cases for the contact class behaviours.
+
+  Args:
+      unittest.TestCase: TestCase class that helps in creating test cases
+  '''
+
