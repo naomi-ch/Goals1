7c14e834a28d73edc11f64fd06dcdbc377b7a72d
Naomi Chika 1 <naomi.chika.1@gmail.com>
2021-05-27 10:21:32 +0300

    create more test cases

--- Core/Week-3/Contact-List/contact.py
+++ Core/Week-3/Contact-List/contact.py
@@ -4,8 +4,8 @@ class Contact:
   Class that generates new instances of contacts
   """
 
-  contact_list = [] 
-
+  contact_list = [] # Empty contact list
+ 
   def __init__(self,first_name,last_name,phone_number,email):
 
     
@@ -14,7 +14,40 @@ class Contact:
         self.phone_number = phone_number
         self.email = email 
       
-    
+  def save_contact(self):
+
+      '''
+      save_contact method saves contact objects into contact_list
+      '''
+
+      Contact.contact_list.append(self)
+
+  def delete_contact(self):
+
+        '''
+        delete_contact method deletes a saved contact from the contact_list
+        '''
+
+        Contact.contact_list.remove(self)
+
+
+  @classmethod
+  def find_by_number(cls,number):
+      '''
+      Method that takes in a number and returns a contact that matches that number
+
+      Args:
+            number: Phone number to search for
+      Returns:
+            Contact of person that matches the number
+      '''
+
+      for contact in cls.contact_list:
+            if contact.phone_number == number:
+                  return contact
+
+
+
 
 
     
--- Core/Week-3/Contact-List/contact_test.py
+++ Core/Week-3/Contact-List/contact_test.py
@@ -17,6 +17,13 @@ class TestContact(unittest.TestCase):
       '''
       self.new_contact = Contact("James","Muriuki","0712345678","james@moringaschool.com") #create contact object
 
+  def tearDown(self):
+        '''
+        tearDown method that does clean up after each test has run
+        '''
+
+        Contact.contact_list = []
+
   def test_init(self):
       ''' 
       test_init test case to test if the object is initialized properly
@@ -26,6 +33,52 @@ class TestContact(unittest.TestCase):
       self.assertEqual(self.new_contact.last_name, "Muriuki")
       self.assertEqual(self.new_contact.phone_number,"0712345678")
       self.assertEqual(self.new_contact.email, "james@moringaschool.com")
+
+
+  def test_save_contact(self):
+      '''
+      test_save_contact test case to test if the contact object is saved into the contact list
+      '''
+
+      self.new_contact.save_contact() # saving the new contact
+      self.assertEqual(len(Contact.contact_list),1)
+
+  def test_save_multiple_contact(self):
+      '''
+      test_save_multiple_contact test case to check if we can save multiple contact objects to our contact list
+      '''
+
+      self.new_contact.save_contact() 
+      test_contact = Contact("Test", "user", "0712345678","test@user.com") #new contact
+      test_contact.save_contact()
+      self.assertEqual(len(Contact.contact_list),2)
+
+  def test_delete_contact(self):
+       '''
+       test_delete_contact to test if we can remove a contact from our contact list
+       '''
+       self.new_contact.save_contact()
+       test_contact = Contact("Test", "user", "0712345678", "test@user.com")
+       test_contact.save_contact()
+
+       self.new_contact.delete_contact() # Deleting a contact object
+       self.assertEqual(len(Contact.contact_list),1)
+
+  def test_find_contact_by_number(self):
+       '''
+       test to check if we can find a contact by number and display information
+       '''
+       self.new_contact.save_contact()
+       test_contact = Contact("Test", "user", "0712345678", "test@user.com")
+       test_contact.save_contact()
+
+       found_contact = Contact.find_by_number("0712345678")
+       self.assertEqual(found_contact.email, test_contact.email)
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
