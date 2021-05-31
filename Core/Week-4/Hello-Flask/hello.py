d6fd503e4d834bb519b9f347a65e0143b5d802cb
Naomi Chika 1 <naomi.chika.1@gmail.com>
2021-05-31 15:30:28 +0300

    add route and index etc

--- Core/Week-4/Hello-Flask/hello.py
+++ Core/Week-4/Hello-Flask/hello.py
@@ -1,2 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask # from flask module we imported Flask Class
+app = Flask(__name__) # created an instance of that class called 'app' by passing in the __name__ variable
+# Flask uses the __name__ variable to determine the root path for the application
+
+@app.route('/') #app.route() is a decorator. We have defined the route for the index page
+def index(): #index() is a VIEW FUNCTION. It is the handler for the route defined above
+    return '<h1> Hello World </h1>'
+
+if __name__ == '__main__':
+    app.run(debug = True) #run() method that launches flask development server
