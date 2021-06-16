930c5f96ce0bba6bac793c8be2f22b1b804271cb
Naomi Chika 1 <naomi.chika.1@gmail.com>
2021-06-16 14:33:33 +0300

    create '/logout' auth.route

--- Core/WEEK-5/Pitches/app/auth/views.py
+++ Core/WEEK-5/Pitches/app/auth/views.py
@@ -20,3 +20,10 @@ def login():
 
     title = "Pitches Login"
     return = render_template('auth/login.html', login_form = login_form, title = title)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
