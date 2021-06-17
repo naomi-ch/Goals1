b4587caa0d5e01ea7454ec88e6a9a59eeaaf475f
Naomi Chika 1 <naomi.chika.1@gmail.com>
2021-06-17 11:46:10 +0300

    create a pitchform

--- Core/WEEK-5/Pitches/app/main/forms.py
+++ Core/WEEK-5/Pitches/app/main/forms.py
@@ -2,6 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required 
 
+class PitchForm(FlaskForm):
+    title = TextAreaField('Your pitch title',validators=[Required()])
+    pitch = TextAreaField('Your pitch',validators=[Required()])
+    submit = SubmitField('Submit')
+
 class CommentForm(FlaskForm):
 
     #title = Stringfield('Comment title',validators=[Required()]) #should there be a comment title? 
